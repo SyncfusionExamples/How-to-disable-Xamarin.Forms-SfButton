eeb822d8f8ccf3cc843b2b2db3a79d5f65c46c11
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-18 10:26:57 +0530

    Update new version.

---
+++ SimpleSample/SimpleSample/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SimpleSample
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private bool isButtonEnabled = false;
+        public Command ButtonCommand { get; set; }
+
+        public bool IsButtonEnabled
+        {
+            get { return isButtonEnabled; }
+            set
+            {
+                isButtonEnabled = value;
+                ButtonCommand.ChangeCanExecute();
+                OnPropertyChanged();
+            }
+        }
+
+        bool CanExecuteClickCommand(object arg)
+        {
+            return isButtonEnabled;
+        }
+
+        public ViewModel()
+        {
+            ButtonCommand = new Command(ExecuteClickCommand, CanExecuteClickCommand);
+        }
+
+
+        void ExecuteClickCommand(object obj)
+        {
+            //Execute Xamarin.Forms SfButton Command action.
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
